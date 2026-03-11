ec69d9f6fa6964055249a3e730e6ca5156bcdf67
Nicolas Righi <nicolasrighir@gmail.com>
2026-03-11 16:02:26 -0300

    atualizações

---
+++ js/modules/modal.js
@@ -0,0 +1,15 @@
+export default function initModal(){
+
+    const botaoAbrir = document.querySelector('[data-modal="abrir"]')
+    const botaoFechar = document.querySelector('[data-modal="fechar"]')
+    const containerModal =  document.querySelector('[data-modal="container"]')
+  
+
+     function clicou(){
+        containerModal.classList.add('ativo')
+     }
+
+     botaoAbrir.addEventListener('click',clicou)
+    
+}
+
